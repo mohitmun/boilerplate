5c30c33b5518896b615a9d7247be95943d2f9dbb
Mohit M <mohitcrox@gmail.com>
2017-07-12 14:52:45 +0530

    mc

--- db/migrate/20161204075916_add_admin_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddAdminToUser < ActiveRecord::Migration
-  def change
-    add_column :monologue_users, :admin, :boolean
-  # This migration comes from monologue (originally 20120114001001)
-  end
-end
